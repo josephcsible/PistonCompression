5ca951b059f9c0e0cdfd5af171ffde04a80c5733
Joseph C. Sible <josephcsible@users.noreply.github.com>
2017-07-23 10:44:27 -0400

    Bump version

--- src/main/java/josephcsible/pistoncompression/PistonCompression.java
+++ src/main/java/josephcsible/pistoncompression/PistonCompression.java
@@ -59,7 +59,7 @@ public class PistonCompression
 {
 	// XXX duplication with mcmod.info and build.gradle
 	public static final String MODID = "pistoncompression";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.1.0";
 
 	protected static final String[] DEFAULT_REPLACEMENTS = {
 		"minecraft:ice * minecraft:packed_ice",
